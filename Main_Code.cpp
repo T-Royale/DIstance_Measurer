ecb2dd23099c4351ee9fe7b4f05ed629824c17d1
T_Royale <141929186+T-Royale@users.noreply.github.com>
2023-11-04 17:05:34 +0100

    V-1.4

--- Main_Code.cpp
+++ Main_Code.cpp
@@ -1,5 +1,6 @@
-#include <LiquidCrystal.h>
+#include <LiquidCrystal.h> //make sure to install this library!//
 
+//LCD pins//
 #define RS 4
 #define ENABLE 5
 #define D4 6
@@ -7,16 +8,19 @@
 #define D6 8
 #define D7 9
 
+//Sensor Pins//
 #define trigger 3
 #define echo 2
 #define ConValue 58.2
 
+//Create variables//
 LiquidCrystal lcd (RS, ENABLE, D4, D5, D6, D7);
 int duracion;
 int distancia;
 int metros;
 int cm;
 
+//setup//
 void setup() {
   lcd.begin (16,2);
   pinMode(trigger, OUTPUT);
@@ -24,8 +28,7 @@ void setup() {
   Serial.begin(9600);
 }
 
-
-
+//Loop//
 void loop() {
   lcd.setCursor (0, 0);
   digitalWrite(trigger, HIGH);
